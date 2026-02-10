e1d899708d79d6af1016aab6bdeb8ceccb8780d0
Albin B Thomas <albinbthomas8800@gmail.com>
2026-02-10 20:26:57 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT 
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
