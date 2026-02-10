c43aa398bafccc283131c041d31fcd6d8abc2510
Albin B Thomas <albinbthomas8800@gmail.com>
2026-02-11 00:16:33 +0530

    fix: cors

--- server.js
+++ server.js
@@ -7,8 +7,23 @@ import productRoutes from './routes/productRoutes.js'
 
 const app = express();
 
+const allowedOrigins = [
+    "http://localhost:3000/",
+    "https://timelessluxe.vercel.app/"
+]
 
-app.use(cors());
+
+app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }))
+  
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
