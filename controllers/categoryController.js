b9cbe7e8256f339d58efb496e3ba7d45bd10398a
Albin B Thomas <albinbthomas8800@gmail.com>
2026-02-16 01:19:59 +0530

    feat: delete category api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -69,3 +69,28 @@ export const updateCategory = async (req, res) => {
          });
     }
 }
+
+export const deleteCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const category = await Category.findById(id);
+
+        if(!category) {
+            return res.status(404).json({
+                message: "Category not found"
+            });
+        }
+
+        await Category.findByIdAndDelete(id);
+
+        res.status(200).json({
+            message: "Category Deleted Succesfully"
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Failed to delete Category"
+        })
+    }
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import upload from "../middleware/upload.js"
-import { addCategory, getCategories, updateCategory } from "../controllers/categoryController.js";
+import { addCategory, deleteCategory, getCategories, updateCategory } from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.post("/", upload.single("image"), addCategory);
 router.get("/", getCategories);
 router.put("/:id", upload.single("image"), updateCategory);
+router.delete("/:id", deleteCategory);
 
 export default router;
