086bad0a3148305be98420ff8352b212afb8efa9
Albin B Thomas <albinbthomas8800@gmail.com>
2026-02-19 16:59:35 +0530

    feat: Product update and delete Apis

--- controllers/productController.js
+++ controllers/productController.js
@@ -103,3 +103,88 @@ export const getProducts = async (req, res) => {
         })
     }
 }
+
+export const updateProduct = async (req, res) => {
+   try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if(!products) {
+        return res.status(404).json({
+            message: "Product not Found"
+        });
+    }
+
+    const {
+            name,
+            brand,
+            categoryId,
+            price,
+            mrp,
+            description,
+            stock,
+        } = req.body
+
+    const isPopular = JSON.parse(req.body.isPopular || "false");
+    const isWidest = JSON.parse(req.body.isWidest || "false");
+
+        product.name = name;
+        product.brand = brand;
+        product.categoryId = categoryId;
+        product.price = price;
+        product.mrp = mrp;
+        product.description = description;
+        product.stock = stock;
+        product.isPopular = isPopular;
+        product.isWidest = isWidest;
+
+        if(req.file) {
+            product.image = req.file.path;
+        }
+
+        await product.save();
+
+        res.status(200).json({
+            message: "Product updated Succesfully",
+            product,
+        });
+
+   } catch (error) {
+    res.status(500).json({
+        message: "Failed to Update Product",
+        error: error.message,
+    })
+   }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findById(id);
+
+        if(!product) {
+            return res.status(404).json({
+                message: "Product not Found"
+            });
+        }
+
+        const category = await Category.findById(product.categoryId);
+
+        if(category && category.count > 0) {
+            category.count -= 1;
+            await category.save();
+        }
+
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({
+            message: "Product deleted Succesfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to delete product",
+            error: error.message,
+        })
+    }
+}
