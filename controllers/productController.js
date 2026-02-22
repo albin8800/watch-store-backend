8bbc467154e84b419ed743ca3831765e58f25a49
Albin B Thomas <albinbthomas8800@gmail.com>
2026-02-22 14:07:08 +0530

    feat: product by id api

--- controllers/productController.js
+++ controllers/productController.js
@@ -104,6 +104,30 @@ export const getProducts = async (req, res) => {
     }
 }
 
+export const getProeductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findById(id).populate("categoryId", "name");
+        
+        if(!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+
+        }
+
+        res.status(200).json({
+            product,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to fetch product",
+            error: error.message,
+        })
+    }
+}
+
 export const updateProduct = async (req, res) => {
    try {
     const { id } = req.params;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import upload from '../middleware/upload.js'
-import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getProducts, getProeductById, updateProduct } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.post("/", upload.single("image"), createProduct);
 router.get("/", getProducts);
+router.get("/:id", getProeductById);
 router.put("/:id", upload.single("image"), updateProduct);
 router.delete("/:id", deleteProduct);
 
